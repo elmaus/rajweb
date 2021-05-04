e1d71eb6a258bd77c2a3184285848e04de250600
Samuel Camus <42428075+elmaus@users.noreply.github.com>
2021-05-04 04:27:42 +0300

    Update email.js

--- docs/email.js
+++ docs/email.js
@@ -9,9 +9,9 @@ function sendEmail() {
     let message = "Message:\n ${document.getElementById("name")}";
     
     Email.send({
-        Host:"smtp.gmail.com",
-        Username:"elmauscamus@gmail.com", 
-        Password:"1-Love-Codin6",
+        
+        
+        SecurityToken:"5096f11a-ee33-4fc3-b2d6-f75dfc358c2",
         To:"fortempoweb@gmail.com",
         From:"samuel-camus.com",
         Subject:"Testing Sending Email", 
