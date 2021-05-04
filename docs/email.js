3e41a0f5bbc53ddba0cbee368d20f962c6835828
Samuel Camus <elmauscamus@gmail.com>
2021-05-04 07:34:36 +0300

    Merge branch 'main' of https://github.com/elmaus/rajweb into main

--- docs/email.js
+++ docs/email.js
@@ -2,35 +2,20 @@ const btn = document.querySelector('button');
 
 
 function sendEmail() {
-    let name = "Name: ${document.getElementById("name").value} ";
-    let surname = "Surname : ${document.getElementById("surname").value}\n";
-    let email = "Email: ${document.getElementById("email").value}";
-    let number = "Phone Number: ${document.getElementById("number").value}\n";
-    let message = "Message:\n ${document.getElementById("name")}";
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("number").value;
+    let message = document.getElementById("name");
     
     Email.send({
+        SecureToken:"9e85c1e9-cb45-4251-97b2-e06eba0d5077",
         To:"fortempoweb@gmail.com",
         From:"elmauscamus@gmail.com",
         Subject:"Testing Sending Email", 
-        Body: name + surname + email + number + message;
+        Body: "Name: ${name} ${surname}\nEmail: ${email}\nPhone Number: ${number}\nMessage:\n${message}"
     }) 
 
 }
 
 btn.addEventListener('click', sendEmail);
-
-/*
-btn.addEventListener('click', () => (
-    
-    Email.send({
-        host: "smtp.mailtrap.io",
-        Username: "skdkfjskjdk",
-        password:"ksjdfkjsdkfjsk",
-        to: "",
-        From:inputs.elements["email"].value,
-        Subject:"Contact Us Query By Cusomer",
-        Body:inputs.elements["message"].value + "<br>" + inputs.element["name"].value + "<br>" + insputs.elements["phone"]
-    }).then(msg => alert("The email successfully sent"))
-
-))
-*/
